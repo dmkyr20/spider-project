6ebdaf8b96bce57e34fb900d6ba283a0ee3bb4d5
Dima <dmkyr20@gmail.com>
2018-11-24 10:38:22 +0200

    Initial commit

---
+++ app/src/main/java/com/example/dk200/hexapod/BluetoothConnect.java
@@ -0,0 +1,13 @@
+package com.example.dk200.hexapod;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BluetoothConnect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth_connect);
+    }
+}
